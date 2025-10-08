d36d7f0f55ff8f8156645cb291e13631f116fe29
180261 <168944685+180261@users.noreply.github.com>
2025-10-08 08:21:46 +0700

    fix analysis navigation

--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -10,7 +10,7 @@ const navigation = [
   { name: "Chủ nghĩa Quốc xã là gì?", href: "/what-is-nazism" },
   { name: "Lịch sử", href: "/history" },
   { name: "Tội ác & Hậu quả", href: "/atrocities" },
-  { name: "Phân tích & Phê phán", href: "/Analysis" },
+  { name: "Phân tích & Phê phán", href: "/analysis" },
   { name: "Tài liệu tham khảo", href: "/references" },
   { name: "Trò chơi trí nhớ", href: "/memory-game" },
   { name: "Trò chơi mốc thời gian", href: "/timeline-escape" },
